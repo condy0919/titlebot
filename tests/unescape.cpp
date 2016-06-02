639243006b8194bf77e2b413ed1d60018961a995
condy0919 <condy0919@gmail.com>
2016-06-03 02:56:51 +0800

    Add HtmlEntitiesDecode.

---
+++ tests/unescape.cpp
@@ -0,0 +1,22 @@
+#define CATCH_CONFIG_MAIN
+#include "../utils/entities.hpp"
+#include <catch.hpp>
+#include <string>
+#include <iostream>
+
+TEST_CASE("HTMLEntitiesDecoder", "[HTMLEntitiesDecoder]") {
+    std::string str;
+
+    SECTION("&amp;") {
+        str = Html::Unescape("&amp;");
+        REQUIRE(str == "&");
+    }
+    SECTION("&lt;") {
+        str = Html::Unescape("&lt;");
+        REQUIRE(str == "<");
+    }
+    SECTION("&mu;") {
+        str = Html::Unescape("&mu;");
+        REQUIRE(str == "μ");
+    }
+}
---
+++ utils/entities.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace Html {
+std::string Unescape(std::string in);
+}
