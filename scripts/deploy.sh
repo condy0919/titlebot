c02e81291fe297356ceb7f60624fa1f97e9dc986
condy0919 <condy0919@gmail.com>
2016-06-09 22:14:44 +0800

    Minor Update.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!bash
 
+set -e
+
 mkdir -p build
 cd build
 cmake -DTITLEBOT_NICK=anotitlebot \
       -DTITLEBOT_USER=anotitlebot \
       -DTITLEBOT_CHANNELS="#xiyoulinux,#linuxba,##shuati" ..
 make && make test
-cd ..
