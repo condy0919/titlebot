d13f55beca88a0566a6dd9202838b2eb59bf9834
condy0919 <condy0919@gmail.com>
2016-06-09 19:20:42 +0800

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!bash
+
+(mkdir -p build
+ cd build
+ cmake -DTITLEBOT_NICK=anotitlebot \
+       -DTITLEBOT_USER=anotitlebot \
+       -DTITLEBOT_CHANNELS="#xiyoulinux,#linuxba,##shuati" ..
+ make
+ make test
+)
+cp build/titlebot .
