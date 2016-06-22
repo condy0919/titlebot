4707c6d6c52c6d3b0fc22b8906d81bee93bb7879
condy0919 <condy0919@gmail.com>
2016-06-22 23:54:21 +0800

    Fixed: url parsing

--- src/mobot.hpp
+++ src/mobot.hpp
@@ -257,7 +257,7 @@ bool parse_privmsg(std::string s, std::string& from, std::string& target,
             break;
 
         case TOKEN_SLASH_2:
-            if (!std::isspace(c)) {
+            if (std::isalpha(c)) {
                 tmp.push_back(c);
                 st = TOKEN_CONTENT;
             } else {
